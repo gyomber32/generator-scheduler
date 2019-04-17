308fe6bf3eb916e87f8e89cb829ca153bbe61571
Unknown <gyomberattila@gmail.com>
2019-04-17 13:12:59 +0200

    http get poc

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const baseURL = 'http://localhost:3100/api/Weights/getWeight?'
+
+router.get('/weight/quantity=:quantity', function (request, result) {
+  
+  let parameters = {
+    quantity: request.params.quantity
+  }
+
+  const url = baseURL + 'quantity=' + parameters.quantity;
+  var body = '';
+  http.get(url, function (res) {
+    res.on('data', data => {
+      body += data;
+      result.send(body);
+    });
+  });
+});
+
+router.get('/weight=:weight&quantity=:quantity', function (req, res, next) {
+
+  let parameters = {
+    weight: req.params.weight,
+    quantity: req.params.quantity
+  }
+
+  var url = ''
+  if (req.params.weight === undefined) {
+    url = baseURL + 'quantity=' + parameters.quantity;
+  } else {
+    url = baseURL + 'weight=' + parameters.weight + '&' + 'quantity=' + parameters.quantity;
+  }
+
+  http.get(url, function (error) {
+    var body = '';
+    if (error) {
+      res.status(404).send(error);
+      return res;
+    } else {
+      res.on('data', data => {
+        body += data;
+      });
+      res.on('end', () => {
+        body = JSON.parse(body);
+      });
+      res.send(body);
+    }
+  });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const baseURL = 'http://localhost:3100/api/Ages/getAge?'
+
+router.get('/age/quantity=:quantity', function (request, result) {
+  
+  let parameters = {
+    quantity: request.params.quantity
+  }
+
+  const url = baseURL + 'quantity=' + parameters.quantity;
+  var body = '';
+  http.get(url, function (res) {
+    res.on('data', data => {
+      body += data;
+      result.send(body);
+    });
+  });
+});
+
+router.get('/age=:age&quantity=:quantity', function (req, res, next) {
+
+  let parameters = {
+    age: req.params.age,
+    quantity: req.params.quantity
+  }
+
+  var url = ''
+  if (req.params.age === undefined) {
+    url = baseURL + 'quantity=' + parameters.quantity;
+  } else {
+    url = baseURL + 'weight=' + parameters.age + '&' + 'quantity=' + parameters.quantity;
+  }
+
+  http.get(url, function (error) {
+    var body = '';
+    if (error) {
+      res.status(404).send(error);
+      return res;
+    } else {
+      res.on('data', data => {
+        body += data;
+      });
+      res.on('end', () => {
+        body = JSON.parse(body);
+      });
+      res.send(body);
+    }
+  });
 });
 
 module.exports = router;
